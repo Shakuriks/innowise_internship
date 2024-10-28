7b7f2d8c2713030e17ed865b38b93467ab95ef4e
Shakuriks <legonkovnikita77@gmail.com>
2024-10-28 12:48:41 +0300

    feat: export_data_decorator

--- task_1/src/data_exporter.py
+++ task_1/src/data_exporter.py
@@ -11,24 +11,32 @@ class DataExporter:
             db_manager.insert_student(student)
     
     @staticmethod
-    def export_rooms_with_student_count(db_manager, file_format='json'):
-        rooms_with_counts = db_manager.get_rooms_with_student_count()
-        DataExporter._save_data(rooms_with_counts, 'rooms_with_student_count', file_format)
+    def export_data_decorator(method):
+        def wrapper(db_manager, file_format='json'):
+            data = method(db_manager)
+            filename = method.__name__.replace("export_", "")
+            DataExporter._save_data(data, filename, file_format)
+        return wrapper
+    
+    @staticmethod
+    @export_data_decorator
+    def export_rooms_with_student_count(db_manager):
+        return db_manager.get_rooms_with_student_count()
 
     @staticmethod
-    def export_lowest_average_age_rooms(db_manager, file_format='json'):
-        lowest_average_age_rooms = db_manager.get_rooms_with_lowest_average_age()
-        DataExporter._save_data(lowest_average_age_rooms, 'lowest_average_age_rooms', file_format)
+    @export_data_decorator
+    def export_lowest_average_age_rooms(db_manager):
+        return db_manager.get_rooms_with_lowest_average_age()
 
     @staticmethod
-    def export_rooms_with_largest_age_difference(db_manager, file_format='json'):
-        rooms_with_age_difference = db_manager.get_rooms_with_largest_age_difference()
-        DataExporter._save_data(rooms_with_age_difference, 'rooms_with_largest_age_difference', file_format)
+    @export_data_decorator
+    def export_rooms_with_largest_age_difference(db_manager):
+        return db_manager.get_rooms_with_largest_age_difference()
 
     @staticmethod
-    def export_rooms_with_mixed_gender_students(db_manager, file_format='json'):
-        rooms_with_mixed_genders = db_manager.get_rooms_with_mixed_gender_students()
-        DataExporter._save_data(rooms_with_mixed_genders, 'rooms_with_mixed_genders', file_format)
+    @export_data_decorator
+    def export_rooms_with_mixed_gender_students(db_manager):
+        return db_manager.get_rooms_with_mixed_gender_students()
 
     @staticmethod
     def _save_data(data, filename, file_format):
